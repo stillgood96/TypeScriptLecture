adc4f948365dea5282e6a1431a8e88991037d28f
stillgood96 <a787574@naver.com>
2021-10-17 21:18:06 +0900

    TypeScript 2.3 기본타입 정리 1

---
+++ 1-types/1-1-basic.ts
@@ -0,0 +1,18 @@
+{
+  // JavaScript
+
+  // 변수선언 방법
+  // old ver : var
+  // var의 경우 호이스팅 문제 등 안좋은 부분이 다수 있어 쓰면 좋지 못한다.
+  var name01: "이선우";
+  name02: "음하하하";
+
+  // let es6
+  let name02 = "이선우";
+  name02 = "하이하이";
+
+  // const
+  const age = 15;
+
+  // age = 13; nope
+}
